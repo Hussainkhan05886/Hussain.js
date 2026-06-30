40b2567aaca14071fb32fa21f0827236147ec9b0
Muhammad Hussain <hussainofficial2k7@gmail.com>
2026-06-30 12:32:15 +0500

    Comparision Operaters

---
+++ comparision.js
@@ -0,0 +1,18 @@
+console.log (2 > 1); // true
+console.log (2 < 1); // false
+console.log (2 >= 1); // true
+console.log (2 <= 1); // false  
+console.log (2 == 1); // false
+console.log (2 != 1); // true
+
+
+console.log ("2" > 2); // false
+
+// === is syntax for strict equality, it checks both value and type
+console.log ("2" === 1); // false
+console.log ("2" === "2"); // true
+
+// the good method in compasion is both data type should be same, so use === instead of ==
+
+
+// proffesional developers always write clean and readable code they avoid confusion and ambiguity, so they use === instead of ==
