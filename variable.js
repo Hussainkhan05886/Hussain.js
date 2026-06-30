99945c965680b253c8a3627d0ea96d27150f0224
Muhammad Hussain <hussainofficial2k7@gmail.com>
2026-06-30 12:04:50 +0500

    Opperations

--- variable.js
+++ variable.js
@@ -57,4 +57,40 @@ console.log(stringvalue);
 
 console.log(typeof stringvalue);
 
+
+// operations
+
+let a = 10
+
+let negativevalue = -a
+
+console.log(negativevalue);
+
+console.log(2 + 2); // 4
+console.log(2 - 2); // 0
+console.log(2 * 2); // 4
+console.log(2 / 2); // 1
+console.log(2 % 2); // 0   
+console.log(2 ** 2); // 4 power operation
+
+let string1 = "Muhammad"
+let string2 = "Hussain"
+let string3 = string1 + " " + string2
+
+console.log(string3);
+
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+
+
+let num1 = 10
+num1++ // 11
+console.log(num1);
+
+
+
  
