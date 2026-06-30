3285f2dd950f105c0b8dc7e2e1d6e4b0cf77ef34
Muhammad Hussain <hussainofficial2k7@gmail.com>
2026-06-30 11:20:26 +0500

    Conversion of datatypes

--- variable.js
+++ variable.js
@@ -19,5 +19,42 @@
 // undefined
 // object
 // symbol
- console.log(typeof "12" );
+ //console.log(typeof "12" );
+
+
+
+ // conversion operation
+
+
+ let num = "12"
+
+ console.log(typeof num);
+
+ let valueinnum = Number(num)
+ 
+console.log(typeof valueinnum);
+
+// "33" >= 33
+// "33abh" >= NaN
+// true >= 1
+// false >= 0
+
+let isloggedin = 1
+
+let isloggedinboolean = Boolean(isloggedin)
+
+console.log(isloggedinboolean);
+
+// 1 >= true, 0>= false
+// "" >= false
+// "hussain" >= true
+
+let somevalue = 10
+
+let stringvalue = String(somevalue)
+
+console.log(stringvalue);
+
+console.log(typeof stringvalue);
+
  
